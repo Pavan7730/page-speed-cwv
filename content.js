4f7a8fb13bc02fed530759023647f4d8e9294427
Pavan7730 <82305384+Pavan7730@users.noreply.github.com>
2026-01-31 17:07:57 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,19 +1,29 @@
 // content.js
-importScripts();
-
 (function () {
-  function send(metric) {
+  // Avoid running multiple times
+  if (window.__CWV_LOADED__) return;
+  window.__CWV_LOADED__ = true;
+
+  function sendMetric(metric) {
     chrome.runtime.sendMessage({
-      type: "CWV",
+      type: "CWV_METRIC",
       name: metric.name,
-      value: metric.value.toFixed(2)
+      value: Number(metric.value.toFixed(3))
     });
   }
 
-  import('https://unpkg.com/web-vitals@3/dist/web-vitals.iife.js')
-    .then(({ getLCP, getCLS, getINP }) => {
-      getLCP(send);
-      getCLS(send);
-      getINP(send);
-    });
+  // Load web-vitals safely
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/web-vitals@3/dist/web-vitals.iife.js";
+  script.onload = () => {
+    if (!window.webVitals) return;
+
+    const { getLCP, getCLS, getINP } = window.webVitals;
+
+    getLCP(sendMetric);
+    getCLS(sendMetric);
+    getINP(sendMetric);
+  };
+
+  document.documentElement.appendChild(script);
 })();
