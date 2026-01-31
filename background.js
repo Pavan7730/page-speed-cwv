0763eb68f92b57ee8703205743ce67689f05f121
Pavan7730 <82305384+Pavan7730@users.noreply.github.com>
2026-01-31 18:35:47 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
-let cwv = {};
-
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.type === "CWV_METRIC") {
-    cwv[msg.name] = msg.value;
-  }
-});
+let cwvData = {
+  LCP: null,
+  CLS: null,
+  INP: null
+};
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  // Receive CWV metrics from content.js
+  if (msg.type === "CWV_METRIC") {
+    cwvData[msg.name] = msg.value;
+  }
+
+  // Popup asking for data
   if (msg.type === "GET_CWV") {
     sendResponse(cwvData);
   }
